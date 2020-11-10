7d28ad3f2f7a151d18df42510ec39662b05840b4
jeh6043 <69700911+jeh6043@users.noreply.github.com>
2020-11-10 17:24:12 -0500

    Update hw10.py

--- hw10/hw10.py
+++ hw10/hw10.py
@@ -1,4 +1,27 @@
-# Author: Yanling Wang yuw17@psu.edu
-# GitHub ID: yanlingpsu
+# Author: Josiah Harrison jeh6043@psu.edu
+# GitHub ID: jeh6043
+
 def is_anagram(s1, s2):
+  for x in s1:
+    if x.isalnum():
+      if x.lower() not in s2 and x.upper() not in s2:
+        return False
+      else:
+        a = s1.count(x.lower()) + s1.count(x.upper())
+        b = s2.count(x.lower()) + s2.count(x.upper())
+        if a != b:
+          return False
+
   return True
+
+def run():
+  x = input("Enter a string: ")
+  y = input("Enter a string: ")
+  if is_anagram(x,y) == True:
+    print("anagram")
+  else:
+    print("not anagram")
+
+if __name__ == "__main__":
+  run()    
+
